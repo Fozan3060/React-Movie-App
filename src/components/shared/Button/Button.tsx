eb6f7aabc904ecf304f810c493e8337d66d4d643
Fozan3060 <98460987+Fozan3060@users.noreply.github.com>
2024-08-06 04:13:38 +0500

    feat: created shared Button component

---
+++ src/components/shared/Button/Button.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react';
+
+interface ButtonProps {
+  icon?: ReactNode;
+  description?: string;
+  className?: string;
+  children?: ReactNode;
+  onclick?: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  icon,
+  description = 'Button',
+  className = 'bg-slate-200 p-2 text-black rounded-md',
+  children,
+  onclick,
+}) => {
+  return (
+    <button className={className} onClick={onclick}>
+      {description}
+      {children}
+      {icon}
+    </button>
+  );
+};
+
+export default Button;
