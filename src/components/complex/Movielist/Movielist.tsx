1640289ed3ee1bc787435f4fc753377012fc1726
Fozan3060 <98460987+Fozan3060@users.noreply.github.com>
2024-08-06 05:55:33 +0500

    feat: made movielist dynamic using tanstack query

--- src/components/complex/Movielist/Movielist.tsx
+++ src/components/complex/Movielist/Movielist.tsx
@@ -1,33 +1,64 @@
+import axios from 'axios';
 import { useMovie } from '../../../Context/ReactMovieContext';
 import MovieItems from '../../compound/MovieItems/MovieItems';
+import { useQuery } from '@tanstack/react-query';
 
-const moviedata = [
-  {
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BMjk4MWQyZjQtMmY2ZS00YjdmLTgzMDUtYTc5ZDU5NzQzMmJmXkEyXkFqcGdeQXVyOTgwMDkwNzY@._V1_SX300.jpg',
-    Title: 'Interstellar Ranger Commence',
-    Type: 'series',
-    Year: '2022–',
-    imdbID: 'tt11236038',
-  },
-  {
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BMjk4MWQyZjQtMmY2ZS00YjdmLTgzMDUtYTc5ZDU5NzQzMmJmXkEyXkFqcGdeQXVyOTgwMDkwNzY@._V1_SX300.jpg',
-    Title: 'Interstellar Ranger Commence',
-    Type: 'series',
-    Year: '2022–',
-    imdbID: 'tt11236038',
-  },
-];
+interface MovieItem {
+  Poster: string;
+  Title: string;
+  Type: string;
+  Year: string;
+  imdbID: string;
+}
+
+interface MovieResponse {
+  Search: MovieItem[];
+  totalResults: string;
+  Response: string;
+}
+
+const fetchMovie = async (name: string) => {
+  const response = await axios.get(
+    `http://www.omdbapi.com/?apikey=a3d0635c&s=${name}`
+  );
+  return response.data;
+};
 
 const Movielist = () => {
   const { name } = useMovie();
 
+  const { data, isLoading, isError, error } = useQuery<MovieResponse>({
+    queryKey: ['FetchMovie', name],
+    queryFn: () => fetchMovie(name),
+    enabled: !!name,
+  });
+
+  if (isLoading) {
+    return (
+      <h1 className="text-center mt-20 text-2xl font-semibold">Loading ....</h1>
+    );
+  }
+
+  if (isError) {
+    return (
+      <h1 className="text-center mt-20 text-2xl font-semibold">
+        ⛔ {error instanceof Error ? error.message : 'An error occurred'}
+      </h1>
+    );
+  }
+
+  if (!data || !data.Search) {
+    return (
+      <h1 className="text-center mt-20 text-2xl font-semibold">
+        No Movies Found
+      </h1>
+    );
+  }
+
   return (
     <div>
-      <h1>{name}</h1>
-      {moviedata.map((movie) => (
-        <MovieItems movie={movie} type="NonWatched" />
+      {data.Search.map((item) => (
+        <MovieItems key={item.imdbID} type="NonWatched" movie={item} />
       ))}
     </div>
   );
